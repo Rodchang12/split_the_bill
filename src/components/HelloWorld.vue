319219af62722c1f150c46922374f27425931e7d
Rod Chang <a0942248868@gmail.com>
2024-11-14 23:27:00 +0800

    practice

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+      <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -27,10 +27,24 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <p>
+      <RouterLink to="/">about home</RouterLink>
+      <RouterLink to="/about">about</RouterLink>
+    </p>
+
+    <RouterView></RouterView>
   </div>
+
+
+
 </template>
 
 <script>
+import router from '@/router';
+import { RouterLink } from 'vue-router';
+import { RouterView } from 'vue-router';
+
 export default {
   name: 'HelloWorld',
   props: {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import {route} from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(route).mount('#app')
